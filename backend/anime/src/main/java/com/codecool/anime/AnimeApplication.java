1d9deb362493e1c61e2a8cd2a694fccc2dcc7a87
Brigitta Arvay <arvay.brigitta22@gmail.com>
2019-07-30 19:07:15 +0200

    Added builder to entity and added 2 entries to the database.

--- backend/anime/src/main/java/com/codecool/anime/AnimeApplication.java
+++ backend/anime/src/main/java/com/codecool/anime/AnimeApplication.java
@@ -1,19 +1,30 @@
 package com.codecool.anime;
 
+import com.codecool.anime.model.Genre;
+import com.codecool.anime.model.ScreenFun;
+import com.codecool.anime.repository.AnimeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 @EnableSwagger2
 @EnableEurekaClient
 public class AnimeApplication {
+
+    @Autowired
+    private AnimeRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(AnimeApplication.class, args);
@@ -26,6 +37,44 @@ public class AnimeApplication {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/anime/**"))
                 .build();
+    }
+
+    @Bean
+    @Profile("production")
+    public CommandLineRunner init() {
+        return args -> {
+
+            ScreenFun fumoffu = ScreenFun.builder()
+                    .funType("anime")
+                    .genre(Genre.COMEDY)
+                    .title("Full Metal Panic? Fumoffu")
+                    .actors(Arrays.asList("Seki Tomokazu", "Yukino Satsuki", "Yukana"))
+                    .runtime(24)
+                    .rating(8)
+                    .plot("It's back-to-school mayhem with Kaname Chidori and her war-freak classmate Sousuke Sagara.")
+                    .staff(Arrays.asList("Takemoto Yasuhiro", "Hatta Youko"))
+                    .poster("fumoffu.jpg")
+                    .trailer("https://www.youtube.com/embed/rYHyGKZdw2o")
+                    .year(2003)
+                    .build();
+
+            ScreenFun bleach = ScreenFun.builder()
+                    .funType("anime")
+                    .genre(Genre.ACTION)
+                    .year(2004)
+                    .rating(7)
+                    .runtime(24)
+                    .title("Bleach")
+                    .actors(Arrays.asList("Matsuoka Yuki", "Orikasa Fumiko", "Tachiki Fumihiko"))
+                    .staff(Arrays.asList("Abe Noriyuki", "Ishihama Masashi"))
+                    .plot("Ichigo Kurosaki is an ordinary high schooler - until his family is attacked by a Hollow.")
+                    .poster("bleach.jpg")
+                    .trailer("https://www.youtube.com/embed/dg5-XXa0RyE")
+                    .build();
+
+            repository.save(bleach);
+            repository.save(fumoffu);
+        };
     }
 
 }
