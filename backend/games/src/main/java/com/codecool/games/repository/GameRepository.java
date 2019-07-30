15627f76878d904b766956fda79949b114c52d62
mkmdev <mkmdev18@gmail.com>
2019-07-30 13:59:14 +0200

    + list for result of a questionnaire request and result for mood

--- backend/games/src/main/java/com/codecool/games/repository/GameRepository.java
+++ backend/games/src/main/java/com/codecool/games/repository/GameRepository.java
@@ -2,9 +2,15 @@ package com.codecool.games.repository;
 
 import com.codecool.games.model.ScreenFun;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GameRepository extends JpaRepository<ScreenFun, Long> {
 
+    List<ScreenFun> getScreenFunsByGenre_ActionIs();
+
+    ScreenFun getScreenFunsById(@Param("id") long id);
 }
