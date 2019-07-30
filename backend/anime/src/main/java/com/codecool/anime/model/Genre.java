7791095dcd6f08a38c7dc61e9a255973b7cb3c79
Brigitta Arvay <arvay.brigitta22@gmail.com>
2019-07-30 15:08:24 +0200

    Added anime service and models.

---
+++ backend/anime/src/main/java/com/codecool/anime/model/Genre.java
@@ -0,0 +1,18 @@
+package com.codecool.anime.model;
+
+public enum Genre {
+
+    DRAMA,
+    COMEDY,
+    ROMANCE,
+    CRIME,
+    HORROR,
+    THRILLER,
+    DOCUMENTARY,
+    ANIMATED,
+    FANTASY,
+    SCI_FI,
+    ACTION,
+    ADVENTURE
+
+}
