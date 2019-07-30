5c24886b46687d539e5e3ce462729c8d5af27d2e
cskata <katalin.csortos@gmal.com>
2019-07-30 13:49:33 +0200

    Add method signatures to moodservice service and controller

--- backend/moodservice/src/main/java/com/codecool/moodservice/controller/MoodController.java
+++ backend/moodservice/src/main/java/com/codecool/moodservice/controller/MoodController.java
@@ -1,12 +1,12 @@
 package com.codecool.moodservice.controller;
 
+import com.codecool.moodservice.model.Questionnaire;
 import com.codecool.moodservice.model.ScreenFun;
 import com.codecool.moodservice.service.MoodService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/randomizer")
@@ -20,10 +20,24 @@ public class MoodController {
         return this.moodService.getRandomItem();
     }
 
-    @PostMapping("/questionnaire")
-    public void evaluateQuestionnaire() {
-
+    @GetMapping("/checkbox")
+    public List<ScreenFun> getRandomItemByCheckBox() {
+        return this.moodService.getRandomFromEverywhere();
     }
 
+    @GetMapping("/one-from-all")
+    public List<ScreenFun> getOneItemFromEveryType() {
+        return this.moodService.getOneItemFromEveryType();
+    }
+
+    @GetMapping("/all")
+    public List<ScreenFun> getAllItems() {
+        return this.moodService.getAllItems();
+    }
+
+    @PostMapping("/questionnaire")
+    public ScreenFun evaluateQuestionnaire(@RequestBody Questionnaire questionnaire) {
+        return this.moodService.getItemBasedOnQuestionnaire(questionnaire);
+    }
 
 }
--- backend/moodservice/src/main/java/com/codecool/moodservice/service/MoodService.java
+++ backend/moodservice/src/main/java/com/codecool/moodservice/service/MoodService.java
@@ -1,17 +1,45 @@
 package com.codecool.moodservice.service;
 
+import com.codecool.moodservice.model.Questionnaire;
 import com.codecool.moodservice.model.ScreenFun;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MoodService {
 
     @Autowired
+    private AnimeServiceCaller animeServiceCaller;
+
+    @Autowired
+    private GameServiceCaller gameServiceCaller;
+
+    @Autowired
     private MovieServiceCaller movieServiceCaller;
+
+    @Autowired
+    private SeriesServiceCaller seriesServiceCaller;
 
 
     public ScreenFun getRandomItem() {
+        return null;
+    }
+
+    public ScreenFun getItemBasedOnQuestionnaire(Questionnaire questionnaire) {
+        return null;
+    }
+
+    public List<ScreenFun> getRandomFromEverywhere() {
+        return null;
+    }
+
+    public List<ScreenFun> getAllItems() {
+        return null;
+    }
+
+    public List<ScreenFun> getOneItemFromEveryType() {
         return null;
     }
 }
