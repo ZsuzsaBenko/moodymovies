3b7ee01e746dee70f80dfd42dbcbea35595c9cce
mkmdev <mkmdev18@gmail.com>
2019-07-30 10:16:16 +0200

    init

---
+++ backend/games/src/main/java/com/codecool/games/model/Game.java
@@ -0,0 +1,43 @@
+package com.codecool.games.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Game {
+
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    @NotNull
+    private String title;
+
+    private int rating;
+
+    private String genre;
+
+    private List staff;
+
+    private List actors;
+
+    private int year;
+
+    private String plot;
+
+    private int runtime;
+
+    private String poster;
+
+    private String link;
+}
