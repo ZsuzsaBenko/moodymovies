3b2b5e32028eec84301e528d1517b10626e6a0f2
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-07-30 12:49:37 +0200

    setup postgres database

---
+++ backend/series/src/main/java/com/peterpal/demo/model/Series.java
@@ -0,0 +1,24 @@
+package com.peterpal.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Series {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+
+}
