5c5b0c1320fa15c358eb2afc87f2b7ae1821a562
mkmdev <mkmdev18@gmail.com>
2019-07-30 13:39:46 +0200

    added the three endpoints

--- backend/games/src/main/java/com/codecool/games/controller/GameController.java
+++ backend/games/src/main/java/com/codecool/games/controller/GameController.java
@@ -1,9 +1,35 @@
 package com.codecool.games.controller;
 
-import lombok.extern.slf4j.Slf4j;
+import com.codecool.games.model.ScreenFun;
+import com.codecool.games.service.GameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@Slf4j
+@RequestMapping("/games")
+@RequiredArgsConstructor
 public class GameController {
+
+    private final GameService gameService;
+
+    @GetMapping("/random")
+    public ScreenFun getRandomGame() {
+        return gameService.getRandomGame();
+    }
+
+    @GetMapping("/all")
+    public List<ScreenFun> getAllGames() {
+        return gameService.getAllGames();
+    }
+
+    @PostMapping("/questionnaire")
+    public List<ScreenFun> getFilteredGames() {
+        return null;
+        /* TODO */
+    }
 }
