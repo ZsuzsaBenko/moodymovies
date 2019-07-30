8a6be044785b21ac06896bf1971c4086ca070d4e
mkmdev <mkmdev18@gmail.com>
2019-07-30 13:58:05 +0200

    added a Random() bean, restTemplate

--- backend/games/src/main/java/com/codecool/games/GamesApplication.java
+++ backend/games/src/main/java/com/codecool/games/GamesApplication.java
@@ -2,13 +2,17 @@ package com.codecool.games;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Random;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -17,6 +21,17 @@ public class GamesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GamesApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public Random random() {
+        return new Random();
     }
 
     @Bean
