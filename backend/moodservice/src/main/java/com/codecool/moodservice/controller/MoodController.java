3780f616284349eb99af39ab050c43485b2b9804
cskata <katalin.csortos@gmal.com>
2019-07-30 13:33:11 +0200

    Add controller class

--- backend/moodservice/src/main/java/com/codecool/moodservice/controller/MoodController.java
+++ backend/moodservice/src/main/java/com/codecool/moodservice/controller/MoodController.java
@@ -1,8 +1,10 @@
 package com.codecool.moodservice.controller;
 
+import com.codecool.moodservice.model.ScreenFun;
 import com.codecool.moodservice.service.MoodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +15,13 @@ public class MoodController {
     @Autowired
     private MoodService moodService;
 
-    @GetMapping("/all")
-    public void getSg() {
+    @GetMapping("/random-choice")
+    public ScreenFun getRandomItem() {
+        return this.moodService.getRandomItem();
+    }
+
+    @PostMapping("/questionnaire")
+    public void evaluateQuestionnaire() {
 
     }
 
