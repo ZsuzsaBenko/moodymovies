f63826fd74507ddbb9dc5c1beccea09f9095038f
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-07-30 23:25:29 +0200

    Project structure created for the Movies microservice.

---
+++ backend/movies/src/main/java/com/codecool/movies/model/Genre.java
@@ -0,0 +1,17 @@
+package com.codecool.movies.model;
+
+public enum Genre {
+    DRAMA,
+    COMEDY,
+    ROMANCE,
+    CRIME,
+    HORROR,
+    THRILLER,
+    DOCUMENTARY,
+    ANIMATED,
+    FANTASY,
+    SCI_FI,
+    ACTION,
+    ADVENTURE
+
+}
