775c993301dc8b447bb09befba99781ba18f293e
cskata <katalin.csortos@gmal.com>
2019-07-30 13:31:48 +0200

    Add services

--- backend/moodservice/src/main/java/com/codecool/moodservice/service/MoodService.java
+++ backend/moodservice/src/main/java/com/codecool/moodservice/service/MoodService.java
@@ -1,11 +1,17 @@
 package com.codecool.moodservice.service;
 
+import com.codecool.moodservice.model.ScreenFun;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MoodService {
 
+    @Autowired
+    private MovieServiceCaller movieServiceCaller;
 
 
-
+    public ScreenFun getRandomItem() {
+        return null;
+    }
 }
--- backend/moodservice/src/main/java/com/codecool/moodservice/service/SeriesServiceCaller.java
+++ backend/moodservice/src/main/java/com/codecool/moodservice/service/SeriesServiceCaller.java
@@ -1,4 +1,7 @@
 package com.codecool.moodservice.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SeriesServiceCaller {
 }
