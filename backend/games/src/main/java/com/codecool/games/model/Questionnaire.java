14b0754354b297b8f1a50beb7c1503cf10eed06c
mkmdev <mkmdev18@gmail.com>
2019-07-30 13:56:59 +0200

    implemented questionnaire class

---
+++ backend/games/src/main/java/com/codecool/games/model/Questionnaire.java
@@ -0,0 +1,19 @@
+package com.codecool.games.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Questionnaire {
+
+    private int masochist;
+    private int partOfDay;
+    private int gender;
+    private int watchingAlone;
+    private Mood mood;
+    private Genre genre;
+
+}
