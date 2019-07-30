8cdc1118ca0b259f6f02e414cef0193e6bdd82fe
mkmdev <mkmdev18@gmail.com>
2019-07-30 14:01:03 +0200

    implemented basic Gameservice

--- backend/games/src/main/java/com/codecool/games/service/GameService.java
+++ backend/games/src/main/java/com/codecool/games/service/GameService.java
@@ -1,7 +1,32 @@
 package com.codecool.games.service;
 
+import com.codecool.games.model.ScreenFun;
+import com.codecool.games.repository.GameRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Random;
+
 @Service
+@RequiredArgsConstructor
 public class GameService {
+
+    private final GameRepository gameRepository;
+
+    @Autowired
+    private Random random;
+
+    public ScreenFun getRandomGame() {
+        return null;
+    }
+
+    public List<ScreenFun> getAllGames() {
+        return gameRepository.findAll();
+    }
+
+    public List<ScreenFun> getFilteredGames() {
+        return gameRepository.getScreenFunsByGenre_ActionIs();
+    }
 }
