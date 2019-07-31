6fb9b8b70af0444f210560e107c91cfbf27ec408
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-07-31 12:58:17 +0200

    MovieService endpoints work.

--- backend/movies/src/main/java/com/codecool/movies/controller/MovieController.java
+++ backend/movies/src/main/java/com/codecool/movies/controller/MovieController.java
@@ -1,9 +1,37 @@
 package com.codecool.movies.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codecool.movies.model.Questionnaire;
+import com.codecool.movies.model.ScreenFun;
+import com.codecool.movies.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
+    private final MovieService movieService;
+
+    @Autowired
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+
+
+    @GetMapping("/all")
+    public List<ScreenFun> getAllMovies() {
+        return this.movieService.getAllMovies();
+    }
+
+    @PostMapping("/questionnaire")
+    public ScreenFun findMovieBasedOnProfile(@RequestBody Questionnaire questionnaire) {
+        return movieService.findMovieBasedOnProfile(questionnaire);
+    }
+
+    @GetMapping("/random")
+    public ScreenFun getARandomMovie() {
+        return movieService.getARandomMovie();
+    }
 }
