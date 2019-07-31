86f915b5311b67b83c401314f5f9e5785d41d0ee
mkmdev <mkmdev18@gmail.com>
2019-07-31 10:24:49 +0200

    fill up database with action, comedy, horror and documentary games and actors

---
+++ backend/games/src/main/resources/sql/games.sql
@@ -0,0 +1,61 @@
+/*DONT RUN WITHOUT REPLACE ALL THE YOUTUBE LINKS TO EMBED CODES*/
+-- Action games
+INSERT INTO SCREEN_FUN(ID, FUN_TYPE, GENRE, PLOT, POSTER, RATING, RUNTIME, TITLE, TRAILER, YEAR, DIRECTOR, PRODUCER)
+VALUES (1, 'game', 'action', 'An action-adventure video game developed by Rockstar North and published by Rockstar Games.','gtav.png',95.0,100,'GTA V','https://www.youtube.com/embed/hvoD7ehZPcM', 2013, 'Leslie Benzies', 'Imran Sarwar');
+INSERT INTO SCREEN_FUN(ID, FUN_TYPE, GENRE, PLOT, POSTER, RATING, RUNTIME, TITLE, TRAILER, YEAR, DIRECTOR, PRODUCER)
+VALUES (2, 'game', 'action', 'An online multiplayer battle royale game developed and published by PUBG Corporation, a subsidiary of South Korean video game company Bluehole. ','pubg.jpg',80.0,0,'PlayerUnknowns Battlegrounds','https://www.youtube.com/embed/ODWCbu_cuqk', 2017, 'Brendan Greene
+', 'Chang-han Kim');
+INSERT INTO SCREEN_FUN(ID, FUN_TYPE, GENRE, PLOT, POSTER, RATING, RUNTIME, TITLE, TRAILER, YEAR, DIRECTOR, PRODUCER)
+VALUES (3, 'game', 'action', 'Atari 2600 game made by Mystique in 1982 that is widely considered offensive due to its plot involving the apparent rape of a Native American woman.','gtav.png',32.0,20,'Custers Revenge','https://www.youtube.com/embed/hvoD7ehZPcM', 1982, 'Mystique', 'Mystique');
+
+-- Comedy games
+INSERT INTO SCREEN_FUN(ID, FUN_TYPE, GENRE, PLOT, POSTER, RATING, RUNTIME, TITLE, TRAILER, YEAR, DIRECTOR, PRODUCER)
+VALUES (4, 'game', 'comedy', 'The Jackbox Party Pack 4, Drawful 2, and Quiplash are our top selection to play today.','partypack.jpeg',92.0,1200,'The Jackbox Party Pack 5','https://www.youtube.com/embed/uuPY0_8CO6o', 2018, 'Jackbox Games', 'Jackbox Games');
+INSERT INTO SCREEN_FUN(ID, FUN_TYPE, GENRE, PLOT, POSTER, RATING, RUNTIME, TITLE, TRAILER, YEAR, DIRECTOR, PRODUCER)
+VALUES (5, 'game', 'comedy', 'A point-and-click adventure, developed and published by Daedalic Entertainment.','deponia.jpg',6.27,1200,'Deponia','https://www.youtube.com/embed/KRQhKw-YCWs', 2012, 'Daedalic Entertainment', 'WW: Lace International');
+
+-- Horror games
+INSERT INTO SCREEN_FUN(ID, FUN_TYPE, GENRE, PLOT, POSTER, RATING, RUNTIME, TITLE, TRAILER, YEAR, DIRECTOR, PRODUCER)
+VALUES (6, 'game', 'horror', 'Interactive drama and survival horror video game. Players assume control of eight young adults who have to survive on Blackwood Mountain when their lives are threatened.','untildawn.jpg',97.0,200,'Until Dawn','https://www.youtube.com/embed/4bgIZONHPXA', 2012, 'Will Byles
+', 'Pete Samuels');
+INSERT INTO SCREEN_FUN(ID, FUN_TYPE, GENRE, PLOT, POSTER, RATING, RUNTIME, TITLE, TRAILER, YEAR, DIRECTOR, PRODUCER)
+VALUES (7, 'game', 'horror', 'TPS survival horror video game','silenthill.jpg',30.0,80,'Silent Hill 2','https://www.youtube.com/embed/KRQhKw-YCWs', 2001, 'Masashi Tsuboyama', 'Akihiro Imamura');
+
+-- DOCUMENTARY games
+INSERT INTO SCREEN_FUN(ID, FUN_TYPE, GENRE, PLOT, POSTER, RATING, RUNTIME, TITLE, TRAILER, YEAR, DIRECTOR, PRODUCER)
+VALUES (8, 'game', 'DOCUMENTARY', 'A real-time strategy video game. The game requires players to collect resources to construct buildings, produce citizens, and conquer opposing civilizations. Empire Earth spans 500,000 years of world history, and ending with the nano age.','empire.jpg',72.0,120,'Empire Earth','https://www.youtube.com/embed/rP4wCFe1_Lo', 2001, 'Stainless Steel Studios', 'Sierra On-Line');
+INSERT INTO SCREEN_FUN(ID, FUN_TYPE, GENRE, PLOT, POSTER, RATING, RUNTIME, TITLE, TRAILER, YEAR, DIRECTOR, PRODUCER)
+VALUES (9, 'game', 'DOCUMENTARY', 'Online learning game set in a big open world is a cut above.','jumpstart.jpg',20.0,20,'JumpStart 3D Virtual World','https://www.youtube.com/embed/2h9YntthI5U', 2009, 'JumpStart', 'JumpStart');
+
+
+
+INSERT INTO SCREEN_FUN_ACTORS("SCREEN_FUN_ID", "ACTORS")
+VALUES (1,'Dan Houser');
+INSERT INTO SCREEN_FUN_ACTORS("SCREEN_FUN_ID", "ACTORS")
+VALUES (1,'Rupert Humphries');
+INSERT INTO SCREEN_FUN_ACTORS("SCREEN_FUN_ID", "ACTORS")
+VALUES (1,'Michael Unsworth');
+INSERT INTO SCREEN_FUN_ACTORS("SCREEN_FUN_ID", "ACTORS")
+VALUES (2,'Tae-seok Jang');
+INSERT INTO SCREEN_FUN_ACTORS("SCREEN_FUN_ID", "ACTORS")
+VALUES (3,'George Armstrong Custer');
+INSERT INTO SCREEN_FUN_ACTORS("SCREEN_FUN_ID", "ACTORS")
+VALUES (4,'Matulef Jeffrey');
+INSERT INTO SCREEN_FUN_ACTORS("SCREEN_FUN_ID", "ACTORS")
+VALUES (5,'Edna Harvey');
+INSERT INTO SCREEN_FUN_ACTORS("SCREEN_FUN_ID", "ACTORS")
+VALUES (5,'Emily Morganti');
+INSERT INTO SCREEN_FUN_ACTORS("SCREEN_FUN_ID", "ACTORS")
+VALUES (6,'Brandon Kosinski');
+INSERT INTO SCREEN_FUN_ACTORS("SCREEN_FUN_ID", "ACTORS")
+VALUES (6,'Rami Malek');
+INSERT INTO SCREEN_FUN_ACTORS("SCREEN_FUN_ID", "ACTORS")
+VALUES (7,'Akira Yamaoka');
+INSERT INTO SCREEN_FUN_ACTORS("SCREEN_FUN_ID", "ACTORS")
+VALUES (8,'Ed Lima');
+INSERT INTO SCREEN_FUN_ACTORS("SCREEN_FUN_ID", "ACTORS")
+VALUES (8,'Steve Maitland');
+INSERT INTO SCREEN_FUN_ACTORS("SCREEN_FUN_ID", "ACTORS")
+VALUES (9,'Steve Manctuary');
+
+
