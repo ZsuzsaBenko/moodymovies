ee79da3ebf5967f87bddb1f4722ab92366e25f33
cskata <katalin.csortos@gmail.com>
2019-08-01 21:45:52 +0200

    Add sample move to Movie commandlinerunner

--- backend/movies/src/main/java/com/codecool/movies/MoviesApplication.java
+++ backend/movies/src/main/java/com/codecool/movies/MoviesApplication.java
@@ -1,21 +1,31 @@
 package com.codecool.movies;
 
+import com.codecool.movies.model.Genre;
+import com.codecool.movies.model.ScreenFun;
+import com.codecool.movies.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
 import java.util.Random;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableEurekaClient
 public class MoviesApplication {
+
+    @Autowired
+    private MovieRepository movieRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(MoviesApplication.class, args);
@@ -35,4 +45,27 @@ public class MoviesApplication {
         return new Random();
     }
 
+    @Bean
+    @Profile("production")
+    public CommandLineRunner init() {
+        return args -> {
+
+            ScreenFun sample = ScreenFun.builder()
+                    .funType("movie")
+                    .genre(Genre.ACTION)
+                    .year(2004)
+                    .rating(7.88)
+                    .runtime(24)
+                    .title("sample")
+                    .actors(Arrays.asList("sample", "sample", "sample"))
+                    .director("sample")
+                    .producer("sample")
+                    .plot("sample")
+                    .poster("sample.jpg")
+                    .trailer("https://www.youtube.com/embed/dg5-XXa0RyE")
+                    .build();
+
+            movieRepository.saveAll(Arrays.asList());
+        };
+    }
 }
