80cafd4bfe05a5341009b029170b47e2e9d20699
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-08-01 10:58:28 +0200

    Fun-detail component css mostly done.

--- frontend/moodymovies/src/app/components/fun-detail/fun-detail.component.ts
+++ frontend/moodymovies/src/app/components/fun-detail/fun-detail.component.ts
@@ -12,6 +12,7 @@ import {DomSanitizer} from '@angular/platform-browser';
 export class FunDetailComponent implements OnInit {
   chosenFun = new ScreenFun();
   trailerUrl: any;
+  runtime: string;
 
   constructor(private funService: FunService,
               private router: ActivatedRoute,
@@ -22,22 +23,37 @@ export class FunDetailComponent implements OnInit {
     if (this.router.snapshot.url[0].path === 'random-surprise') {
       this.funService.getSurprise().subscribe(response => {
         this.chosenFun = response;
-        this.trailerUrl = this.getSafeUrl(this.chosenFun.trailer);
+        this.setFieldsForDisplay();
       });
     } else if (this.router.snapshot.url[0].path.startsWith('random-profile')) {
       const questionnaire = this.router.snapshot.queryParams;
       this.funService.getProfile(questionnaire).subscribe(response => {
         this.chosenFun = response;
-        this.trailerUrl = this.getSafeUrl(this.chosenFun.trailer);
+        this.setFieldsForDisplay();
       });
     } else {
       this.chosenFun = history.state;
-      this.trailerUrl = this.getSafeUrl(this.chosenFun.trailer);
+      this.setFieldsForDisplay();
     }
   }
 
   getSafeUrl(url) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  setFieldsForDisplay() {
+    this.trailerUrl = this.getSafeUrl(this.chosenFun.trailer);
+
+    this.chosenFun.genre = this.chosenFun.genre.toLowerCase();
+    if (this.chosenFun.genre === 'sci_fi') {
+      this.chosenFun.genre = 'science fiction';
+    }
+
+    if (this.chosenFun.runtime > 60) {
+      this.runtime = `${Math.floor(this.chosenFun.runtime / 60)}h ${this.chosenFun.runtime % 60}min`;
+    } else {
+      this.runtime = `${this.chosenFun.runtime} min`;
+    }
   }
 
 }
