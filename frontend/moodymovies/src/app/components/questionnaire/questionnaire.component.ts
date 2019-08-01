65d6f64bb8d73178c371d0bddc040e224bf19005
cskata <katalin.csortos@gmail.com>
2019-08-01 19:50:10 +0200

    Questionnaire got default values

--- frontend/moodymovies/src/app/components/questionnaire/questionnaire.component.ts
+++ frontend/moodymovies/src/app/components/questionnaire/questionnaire.component.ts
@@ -24,13 +24,13 @@ export class QuestionnaireComponent implements OnInit {
 
   onSubmit() {
     const questionnaire = new Questionnaire();
-    questionnaire.gender = parseInt(this.gender, 10);
+    questionnaire.gender = this.gender != null ? parseInt(this.gender, 10) : 1;
     questionnaire.age = (this.age >= 0 && this.age <= 100) ? this.age : 18;
-    questionnaire.horoscope = (this.horoscope !== '') ? this.horoscope : 'AQUARIUS';
-    questionnaire.alone = parseInt(this.alone, 10);
-    questionnaire.timeOfDay = parseInt(this.timeOfDay, 10);
-    questionnaire.mood = (this.mood !== '') ? this.mood : 'LAUGH';
-    questionnaire.masochist = parseInt(this.masochist, 10);
+    questionnaire.horoscope = this.horoscope != null ? this.horoscope : 'AQUARIUS';
+    questionnaire.alone = this.alone != null ? parseInt(this.alone, 10) : 1;
+    questionnaire.timeOfDay = this.timeOfDay != null ? parseInt(this.timeOfDay, 10) : 1;
+    questionnaire.mood = this.mood != null ? this.mood : 'LAUGH';
+    questionnaire.masochist = this.masochist != null ? parseInt(this.masochist, 10) : 1;
 
     const navigationExtras: NavigationExtras = {
       queryParams: questionnaire
